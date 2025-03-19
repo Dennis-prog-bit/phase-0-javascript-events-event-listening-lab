42374aa3381756fcfbd5aac516863fd589491187
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-03-19 11:40:30 +0300

    final

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const button = document.getElementById("button"); 
+    if (button) {
+        button.addEventListener("click", function () {
+            console.log("Button was clicked!");
+        });
+    } else {
+        console.error("Element with ID 'button' not found!");
+    }
 }
+
+addingEventListener();
+
